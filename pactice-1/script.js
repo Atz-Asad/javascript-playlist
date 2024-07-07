d727bedde81d539ff1142166d008f9740da9a00b
asad <105508686+Atz-Asad@users.noreply.github.com>
2024-07-08 02:12:05 +0600

    prist day learn javascript sclectoor
    prist day learn javascript sclectoor

---
+++ pactice-1/script.js
@@ -0,0 +1,24 @@
+// let id1 = document.getElementById("id1");
+// id1.innerHTML = "learn javascript id succes";
+
+
+// let classname1 = document.getElementsByClassName("classname1")[0];
+// classname1.innerHTML = "learn class name succes";
+
+
+// let attibute5 = document.getElementsByName("attibute5")[0];
+// attibute5.innerHTML = "heading attune change";
+
+// let heading1 = document.getElementsByTagName("h1")[4];
+
+// heading1.innerHTML = "heading one change";
+
+// let id1 = document.querySelectorAll('#id1')[2];
+
+// id1.innerHTML = "this id change";
+
+
+function myFunction() {
+  document.getElementById("demo1").innerHTML = "Hello Dolly!";
+  document.getElementById("demo2").innerHTML = "How are you?";
+}
