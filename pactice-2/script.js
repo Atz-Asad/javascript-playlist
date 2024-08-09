ae50ec9e2cec692e0728f6a3f4bba1a453e1277a
asad <105508686+Atz-Asad@users.noreply.github.com>
2024-08-09 12:44:28 +0600

    playlist add

---
+++ pactice-2/script.js
@@ -0,0 +1,51 @@
+// let id1 = document.getElementById("id1");
+// id1.innerHTML = "learn javascript id succes";
+
+
+// let classname1 = document.getElementsByClassName("classname1")[0];
+// classname1.innerHTML = "learn class name succes";
+
+
+// let attibute5 = document.getElementsByName("attibute5")[0];
+// attibute5.innerHTML = "heading attune change";
+
+// let heading1 = document.getElementsByTagName("h1")[4];
+
+// heading1.innerHTML = "heading one change";
+
+// let id1 = document.querySelectorAll('#id1')[2];
+
+// id1.innerHTML = "this id change";
+
+
+// function myFunction() {
+//   document.getElementById("demo1").innerHTML = "Hello Dolly!";
+//   document.getElementById("demo2").innerHTML = "How are you?";
+// }
+
+// var a = 5;
+// var b = 3;
+
+// function asad(){
+
+//   if( a == b ){
+//     console.log(" this is true");
+//   }
+//   else{
+//     console.log( "this is false ");
+//   }
+// }
+// asad();
+
+
+document.querySelectorAll("h1");
+
+// var a;
+
+// var a;
+
+// a = 5;
+
+// a =  6; 
+
+// console.log(a);
